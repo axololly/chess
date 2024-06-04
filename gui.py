d4ca67bcec5358e92e2bd8402eea7eef15f8a87c
axololly <jamesgriggs362@gmail.com>
2024-06-04 23:33:48 +0100

    Added a screen for the pieces to be drawn on

--- gui.py
+++ gui.py
@@ -1,33 +1,33 @@
 import pygame
 
-class GUI:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((640, 640))
-        pygame.display.set_caption("Chess")
-        self.clock = pygame.time.Clock()
+pygame.init()
 
-    def run(self):
-        running = True
+HEIGHT = 480
+WIDTH = 480
 
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    running = False
+pygame.display.set_caption("Chess")
 
-            for i in range(8):
-                for j in range(8):
-                    color = (50, 168, 82) if (i + j) % 2 else (191, 246, 195)
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+
+    for i in range(8):
+        for j in range(8):
+            color = (50, 168, 82) if (i + j) % 2 else (191, 246, 195)
                         
-                    pygame.draw.rect(self.screen, color, (i * 80, j * 80, 80, 80))
+            pygame.draw.rect(screen, color, (i * WIDTH // 8, j * HEIGHT // 8, WIDTH // 8, HEIGHT // 8))
 
-            pygame.display.flip()
+    pygame.display.flip()
             
-            self.clock.tick(60)
+    clock.tick(60)
 
-        pygame.quit()
-
-GUI().run()
+pygame.quit()
