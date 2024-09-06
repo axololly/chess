2cc6060402c03f04e5003f4d9877fde747f52941
Axololly <jamesgriggs362@gmail.com>
2024-09-06 13:23:46 +0100

    Updated Bitboard.cs to use different PopLSB method

--- Bitboard.cs
+++ Bitboard.cs
@@ -7,19 +7,43 @@ namespace Bitboards
     {
         private ulong bitboard;
 
+        public static ulong Filled { get { return ulong.MaxValue; } }
+
         public Bitboard(ulong bitboard = 0) => this.bitboard = bitboard;
 
         // For changing between types
         public static implicit operator ulong(Bitboard bb) => bb.bitboard;
         public static implicit operator Bitboard(ulong u) => new(u);
-        public static implicit operator bool(Bitboard bb) => bb != 0;
+        public static implicit operator bool(Bitboard bb) => bb.bitboard != 0;
+
+        // For using bitwise operators
+        public static Bitboard operator |(Bitboard first, Bitboard second)
+            => new(first.bitboard | second.bitboard);
+        
+        public static Bitboard operator ^(Bitboard first, Bitboard second)
+            => new(first.bitboard ^ second.bitboard);
+        
+        public static Bitboard operator &(Bitboard first, Bitboard second)
+            => new(first.bitboard & second.bitboard);
+        
+        public static Bitboard operator ~(Bitboard bb)
+            => new(~bb.bitboard);
+        
+        public static Bitboard operator >>(Bitboard bb, int shift)
+            => new(bb.bitboard >> shift);
+        
+        public static Bitboard operator <<(Bitboard bb, int shift)
+            => new(bb.bitboard << shift);
+
 
         // Bit tricks
         public int PopLSB()
         {
+            if (bitboard == 0) throw new Exception("cannot pop LSB off of an empty bitboard.");
+
             int LSB = BitOperations.TrailingZeroCount(bitboard);
             
-            bitboard ^= 1UL << LSB;
+            bitboard &= bitboard - 1;
             
             return LSB;
         }
